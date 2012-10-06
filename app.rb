578caac16017a0c4b7e2f0f8bb8c1534cd688aaf
unknown <ruben@ruben-dt2.tadashi.com>
2012-10-06 14:15:31 -0700

    Reset the app

--- app.rb
+++ app.rb
@@ -1,12 +1,9 @@
 require "sinatra"
 require 'koala'
-require 'base64'
-require 'crack/json'
-
 
 enable :sessions
-set :raise_errors, true
-set :show_exceptions, true
+set :raise_errors, false
+set :show_exceptions, false
 
 # Scope defines what permissions that we are asking the user to grant.
 # In this example, we are asking for the ability to publish stories
@@ -23,30 +20,10 @@ unless ENV["FACEBOOK_APP_ID"] && ENV["FACEBOOK_SECRET"]
 end
 
 
-helpers do
-  def base64_url_decode str
-    encoded_str = str.gsub('-','+').gsub('_','/')
-    encoded_str += '=' while !(encoded_str.size % 4).zero?
-    Base64.decode64(encoded_str)
-  end
-
-  def decode_data(signed_request)
-    encoded_sig, payload = signed_request.split('.')
-    data = base64_url_decode(payload)
-  end
-end
-
-
 get "/" do
 
-  @encoded_request = params[:signed_request]
-  @json_request = decode_data(@encoded_request)
-  @signed_request = Crack::JSON.parse(@json_request)
-  if @signed_request['page']['liked']
-    erb :unlocked
-  else
-    erb :locked
-  end
+
+  erb :index
 end
 
 # used by Canvas apps - redirect the POST to be a regular GET
