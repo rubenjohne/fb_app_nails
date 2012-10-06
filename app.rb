e95d13bd2bf69f54164dcd6d20e6405e4876efad
unknown <ruben@ruben-dt2.tadashi.com>
2012-10-06 13:42:45 -0700

    added liked function from Koala 10

--- app.rb
+++ app.rb
@@ -23,8 +23,8 @@ end
 get "/" do
 
   # check if user liked the page first
-  oauth = Koala::Facebook::OAuth.new(ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"])
- @signed_request = oauth.parse_signed_request(params[:signed_request])
+  @oauth = Koala::Facebook::OAuth.new(ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"])
+  @signed_request = oauth.parse_signed_request(params[:signed_request])
 
   erb :index
 end
