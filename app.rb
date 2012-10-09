95de526522acf01bb49017602fde80b5f10ebf40
Ruben Epong <rubenjohne@gmail.com>
2012-10-09 01:24:39 -0700

    test user info

--- app.rb
+++ app.rb
@@ -76,8 +76,6 @@ before do
 end
 
 post "/" do
-  # will add  a function later to check if the user liked the page then redirect to unlocked or locked page
-  @title = "Vote for Nail Art"
   signed_request = params[:signed_request]
   oauth = Koala::Facebook::OAuth.new(ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"])
   @signed_request = oauth.parse_signed_request(signed_request)
@@ -154,8 +152,10 @@ end
 
 get '/vote/:id' do
   art = Art.get(params[:id])
+  @graph = Koala::Facebook::API.new
+  @user_id = @signed_request['user_id']
+  @user = @graph.get_object(@user_id)
   art.votes.create(:ip_address => env["REMOTE_ADDR"], :voted_by => "Ruben")
-  @title ="Thank you for voting!"
   @message ="Thanks for voting #{params[:voted_by]}" unless params[:voted_by].nil?  
   @arts = Art.all(:order => [:created_at.desc])
   erb :unlocked
