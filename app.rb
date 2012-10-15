efeb5589c4a3b27b50ede7cf3bf9789458aa0a9d
Ruben Epong <rubenjohne@gmail.com>
2012-10-15 02:13:15 -0700

    fixed get_object

--- app.rb
+++ app.rb
@@ -238,9 +238,9 @@ end
 get '/auth/facebook/callback' do
   session[:access_token] = authenticator.get_access_token(params[:code])
   @graph = Koala::Facebook::API.new(session[:access_token])  
-  @user = @graph.get_objects("me")  
+  @user = @graph.get_object("me")  
   if @user.nil?
-    @script_location = "<script>window.top.location.href='https://graph.facebook.com/oauth/authorize?client_id=" + ENV["FACEBOOK_APP_ID"] + "&redirect_uri=http://frozen-thicket-2524.herokuapp.com/';</script>"
+    @script_location = "<script>top.location.href='https://graph.facebook.com/oauth/authorize?client_id=" + ENV["FACEBOOK_APP_ID"] + "&redirect_uri=http://frozen-thicket-2524.herokuapp.com/';</script>"
     erb :authenticated
   else 
     redirect "/"
