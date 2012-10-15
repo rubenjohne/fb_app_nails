663a5320bc0fffd44a883d168b35af3ec666f436
Ruben Epong <rubenjohne@gmail.com>
2012-10-15 11:44:14 -0700

    check aunthenticator url

--- app.rb
+++ app.rb
@@ -232,7 +232,8 @@ end
 
 get '/auth/facebook' do
   session[:access_token] = nil
-  redirect authenticator.url_for_oauth_code(:permissions => FACEBOOK_SCOPE)
+  @script_location = "<script>window.top.location='" + authenticator.url_for_oauth_code(:permissions => FACEBOOK_SCOPE) + "';</script>"
+  erb :authenticated
 end
 
 get '/auth/facebook/callback' do
@@ -242,7 +243,7 @@ get '/auth/facebook/callback' do
   #if session[:access_token]
   #  redirect "/"    
   #else 
-  @script_location = "<script>window.top.location='https://graph.facebook.com/oauth/authorize?client_id=" + ENV["FACEBOOK_APP_ID"] + "&redirect_uri=http://frozen-thicket-2524.herokuapp.com/';</script>"
+  #@script_location = "<script>window.top.location='https://graph.facebook.com/oauth/authorize?client_id=" + ENV["FACEBOOK_APP_ID"] + "&redirect_uri=http://frozen-thicket-2524.herokuapp.com/';</script>"
   #  @script_location = "<script>top.location.href='https://graph.facebook.com/oauth/authorize?client_id=" + ENV["FACEBOOK_APP_ID"] + "&redirect_uri=http://frozen-thicket-2524.herokuapp.com/';</script>"
     erb :authenticated
   #end  
